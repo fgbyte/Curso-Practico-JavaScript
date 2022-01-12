ff1c12c039c7850ae8307a9f5dfdd0e04e5d60b0
fernan21design <fernan21design@gmail.com>
2022-01-12 14:15:17 -0500

    first commit

---
+++ 01 prueba/variables.js
@@ -0,0 +1,15 @@
+//3.
+var Nombre = "Fernando";
+var Apellido = "Garcia";
+var Nombre_de_usuario_en_Platzi = "fernandoindesign";
+var Edad = "28";
+var Correo_electronico = "garciabayonf@gmail.com";
+var Mayor_de_edad = true;
+var Dinero_ahorrado = 200;
+var Deudas = 30;
+
+//4.
+console.log("Nombre completo: " + Nombre + " " + Apellido);
+console.log("Dinero real: " + (Dinero_ahorrado - Deudas));
+// Nombre completo: Fernando Garcia
+// Dinero real: 170
